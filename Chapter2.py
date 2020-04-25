c068318fcf95d4342614c69b6c7c3cff93c4cfcc
Mary Jackson <louesejackson@gmail.com>
2020-04-25 21:57:45 +0100

    switching editors

--- Chapter2.py
+++ Chapter2.py
@@ -1,1 +1,39 @@
-2 + 2 
+Python 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+>>> 8+8
+16
+>>> 6+6*3
+24
+>>> 
+>>> 2+2+2+2
+8
+>>> 8*6
+48
+>>> 10-5+6
+11
+>>> 2+       2
+4
+>>> 
+>>> 5+
+SyntaxError: invalid syntax
+>>> spam = 15
+>>> spam
+15
+>>> spam+5
+20
+>>> spma
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    spma
+NameError: name 'spma' is not defined
+>>> spam
+15
+>>> spam = 15
+>>> spam +5
+20
+>>> spam = 3
+>>> spam + 5
+8
+>>> 
