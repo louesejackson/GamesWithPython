3107f11f9c758ff72829fb2f9884f42a43210ff6
Mary Jackson <louesejackson@gmail.com>
2020-04-26 20:34:14 +0100

    random number guess

--- Chapter3.py
+++ Chapter3.py
@@ -9,3 +9,70 @@ Type "help", "copyright", "credits" or "license()" for more information.
 >>> 'Hello ' + 'World'
 'Hello World'
 >>> 
+===== RESTART: /Users/mary/Desktop/GamesWithPython/GamesWithPython/hello.py ====
+Hello world!
+What is your name?
+Mary
+It is good to meet you, Mary
+>>> 
+===== RESTART: /Users/mary/Desktop/GamesWithPython/GamesWithPython/hello.py ====
+Hello world!
+What is your name?
+Sean
+It is good to meet you, Sean
+>>> 
+>>> 
+===== RESTART: /Users/mary/Desktop/GamesWithPython/GamesWithPython/hello.py ====
+Hello world!
+What is your name?
+Mindy
+It is good to meet you, Mindy
+>>> 
+===== RESTART: /Users/mary/Desktop/GamesWithPython/GamesWithPython/hello.py ====
+Hello world!
+What is your name?
+bob qwerty 554
+It is good to meet you, bob qwerty 554
+>>> 
+===== RESTART: /Users/mary/Desktop/GamesWithPython/GamesWithPython/guess.py ====
+Hello! What is your name?
+Mary
+Well, Mary, I am thinking of a number between 1 and 20.
+Take a guess.
+5
+Your guess is too low.
+Take a guess.
+15
+Your guess is too high.
+Take a guess.
+7
+Take a guess.
+8
+Your guess is too high.
+Take a guess.
+7
+Take a guess.
+6
+Your guess is too low.
+Nope. The umber I was thinking of was 7
+>>> 
+===== RESTART: /Users/mary/Desktop/GamesWithPython/GamesWithPython/guess.py ====
+Hello! What is your name?
+Mary
+Well, Mary, I am thinking of a number between 1 and 20.
+Take a guess.
+15
+Your guess is too high.
+Take a guess.
+8
+Your guess is too low.
+Take a guess.
+10
+Your guess is too low.
+Take a guess.
+12
+Your guess is too low.
+Take a guess.
+14
+Good job, Mary! You guessed my number in 5guesses!
+>>> 
