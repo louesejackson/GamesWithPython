004e2078be3a103b74957fca133e5b53c2041516
Mary Jackson <louesejackson@gmail.com>
2020-04-26 20:36:54 +0100

    Create shell.py

---
+++ shell.py
@@ -0,0 +1,114 @@
+Python 3.8.1 (v3.8.1:1b293b6006, Dec 18 2019, 14:08:53) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> spam = 'hello'
+>>> spam
+'hello'
+>>> 'Hello' + 'World'
+'HelloWorld'
+>>> 'Hello ' + 'World'
+'Hello World'
+>>> 
+===== RESTART: /Users/mary/Desktop/GamesWithPython/GamesWithPython/hello.py ====
+Hello world!
+What is your name?
+Mary
+It is good to meet you, Mary
+>>> 
+===== RESTART: /Users/mary/Desktop/GamesWithPython/GamesWithPython/hello.py ====
+Hello world!
+What is your name?
+Sean
+It is good to meet you, Sean
+>>> 
+>>> 
+===== RESTART: /Users/mary/Desktop/GamesWithPython/GamesWithPython/hello.py ====
+Hello world!
+What is your name?
+Mindy
+It is good to meet you, Mindy
+>>> 
+===== RESTART: /Users/mary/Desktop/GamesWithPython/GamesWithPython/hello.py ====
+Hello world!
+What is your name?
+bob qwerty 554
+It is good to meet you, bob qwerty 554
+>>> 
+===== RESTART: /Users/mary/Desktop/GamesWithPython/GamesWithPython/guess.py ====
+Hello! What is your name?
+Mary
+Well, Mary, I am thinking of a number between 1 and 20.
+Take a guess.
+5
+Your guess is too low.
+Take a guess.
+15
+Your guess is too high.
+Take a guess.
+7
+Take a guess.
+8
+Your guess is too high.
+Take a guess.
+7
+Take a guess.
+6
+Your guess is too low.
+Nope. The umber I was thinking of was 7
+>>> 
+===== RESTART: /Users/mary/Desktop/GamesWithPython/GamesWithPython/guess.py ====
+Hello! What is your name?
+Mary
+Well, Mary, I am thinking of a number between 1 and 20.
+Take a guess.
+15
+Your guess is too high.
+Take a guess.
+8
+Your guess is too low.
+Take a guess.
+10
+Your guess is too low.
+Take a guess.
+12
+Your guess is too low.
+Take a guess.
+14
+Good job, Mary! You guessed my number in 5guesses!
+>>> 
+===== RESTART: /Users/mary/Desktop/GamesWithPython/GamesWithPython/guess.py ====
+Hello! What is your name?
+Mary
+Well, Mary, I am thinking of a number between 1 and 20.
+Take a guess.
+12
+Your guess is too low.
+Take a guess.
+18
+Your guess is too high.
+Take a guess.
+16
+Your guess is too high.
+Take a guess.
+14
+Your guess is too low.
+Take a guess.
+15
+Good job, Mary! You guessed my number in 5 guesses!
+>>> 
+>>> import random
+>>> random.randint(1,20)
+1
+>>> random.randint(1,20)
+13
+>>> random.randint(1,20)
+19
+>>> random.randint(1,20)
+11
+>>> random.randint(1,20)
+15
+>>> random.randint(1,20)
+8
+>>> random.randint(1,20)
+18
+>>> 
